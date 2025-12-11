fcead7b092cd1f347b9475dcacbe90c04f8ee8af
Breyze <ewan.good100@gmail.com>
2025-12-11 11:55:19 +0100

    feature cpu pour le calcul de la charge cpu en cours et mise a jour du README pour documentation

--- monitor.sh
+++ monitor.sh
@@ -3,10 +3,18 @@
 # Auteur: Ewan GOOD
 # Version: 1.0
 
+function show_cpu() {
+    echo "[CPU] Utilisation :"
+    top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}'
+}
+
+
 function main() {
     echo "=== Démarrage du Monitoring ==="
     # Les fonctions seront appelées ici
+    show_cpu
     echo "==============================="
 }
+
 
 main
